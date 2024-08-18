ce2cb8788c683f61425fca6a97016ae4ce0fff49
yrlan-kleber <yrlankleber@gmail.com>
2024-08-18 19:22:58 -0300

    atualizando pagina de preços

--- pages/4_Analise de Precos.py
+++ pages/4_Analise de Precos.py
@@ -5,21 +5,21 @@ from utils.build import build_header
 import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
-from utils.graph import boxplot,scatter,treemap,hist,bar,select_chart,line_graph
+from utils.graph import *
 from utils.build import  top_categories
 
 
 
 build_header(
-    title='Prinmeiras Analises',
-    hdr='# PRIMEIRAS ANALISES E VISUALIZACOES',
+    title='Analise Dos Precos',
+    hdr='# ANALISE DOS PRECOS E SUAS CORRELACOES',
     p='''
         <p> Aqui vamos realizar as primeiras observações dos dados e correlações entre algumas variaveis</p>
     '''
 )
 data = pd.read_parquet('data/price_cars100k.parquet')
 
-
+st.divider()
 
 price_by_years = data.groupby(by='ano')['preco'].mean()
 price_by_years = pd.DataFrame(price_by_years).reset_index()
@@ -47,18 +47,6 @@ veiculos_anos = data['ano'].value_counts().sort_values().reset_index(name='Total
 st.write(px.bar(veiculos_anos, x='ano',y='Total'))
 
 
-
-
-
-# #grafico de barras
-data_ano = data.groupby(['ano'])['preco'].size().reset_index()
-bar(
-    title='GRAFICO DE BARRAS, PRECO X ANO',
-    data = data_ano,
-    x='ano',
-    y='preco',
-    p=''' <p> Observamos que os precos dos veiculos tendem a ser mais caros com a variacao de ano entre 2014 e 2016</p>'''
-)
 
 
 
