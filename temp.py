185a34dcd3449d60250104b0e3eade56e9ab5dba
Silas7005 <ribeirosilas05@gmail.com>
2024-08-26 09:39:42 -0400

    transicao de base (dataset) para melhorias das analises e resultados

--- temp.py
+++ temp.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
 ## Reservados para codigos temp e outros nao executaveis##
 
 px.box(df,x='preco_depreciado')
@@ -136,3 +140,190 @@ def top_cities_models_makes(df, top_cities_n=10, top_models_n=5):
 # Usando a função com o DataFrame carregado
 df_filtrado = top_cities_models_makes(df, top_cities_n=20, top_models_n=10)
 print(df_filtrado)
+
+
+
+
+
+
+
+def table_report(y_test: np.ndarray, previsao: np.ndarray, method:str =''):
+  st.markdown(f'##### Classification report do metodo:  <span style="color: blue">{method}</span>', unsafe_allow_html=True)
+  report = classification_report(y_test, previsao, output_dict=True)
+  classification_data = pd.DataFrame(report).transpose()
+  st.table(classification_data)
+
+
+
+def confusion_graph(y_test, previsao, method:str = ''):
+  st.markdown(f'##### Matriz de Confução do metodo: <span style="color: blue">{method}</span>', unsafe_allow_html=True)
+  labels = sorted(list(set(y_test) | set(previsao)))
+  cm = pd.DataFrame(0, index=labels, columns=labels)
+  for true_label, predicted_label in zip(y_test, previsao):
+      cm.loc[true_label, predicted_label] += 1
+  st.table(cm)
+
+  
+
+def naive_bayes(
+  x_training: np.ndarray, y_training: np.ndarray, x_test: np.ndarray, y_test: np.ndarray, obj_naive= None
+  )-> None:
+  st.markdown('### Resultado do machine learning usando o método Naive Bayes')
+  if not(os.path.isfile('data/naive_bayes.pkl')):
+    obj_naive = GaussianNB()
+    obj_naive.fit(X_training, y_training)
+    with open('data/naive_bayes.pkl', mode='wb') as f:
+      pickle.dump(obj_naive, f)
+  else:
+    with open('data/naive_bayes.pkl', 'rb') as f:
+      obj_naive = pickle.load(f)
+  previsor = obj_naive.predict(X_test)
+  table_report(y_test, previsor, 'Naive Bayes')
+  confusion_graph(y_test, previsor, 'Naive Bayes')
+  #matrix(X_training, X_test, y_training, y_test, mtd=obj_naive)
+
+
+def tree_decision(
+  x_training: np.ndarray, y_training: np.ndarray, x_test: np.ndarray, y_test: np.ndarray
+  )-> None:
+  st.markdown('### Resultado do machine learning usando o método Árvore de decisão')
+  if not(os.path.isfile('data/tree_decision.pkl')):
+    obj_tree_decision = DecisionTreeClassifier(criterion='entropy')
+    obj_tree_decision.fit(X_training, y_training)
+    with open('data/tree_decision.pkl', mode='wb') as f:
+      pickle.dump(obj_tree_decision, f)
+  else:
+    with open('data/tree_decision.pkl', 'rb') as f:
+      obj_tree_decision = pickle.load(f)
+  prevision_tree = obj_tree_decision.predict(x_test)
+  table_report(y_test, prevision_tree,'Árvore de decisão')
+  confusion_graph(y_test, prevision_tree, 'Árvore de decisão')
+
+  
+def random_forest(
+  x_training: np.ndarray, y_training: np.ndarray, x_test: np.ndarray, y_test: np.ndarray
+  )-> None:
+  st.markdown('### Resultado do machine learning usando o método Random Forest')
+
+  if not(os.path.isfile('data/random_forest.pkl')):
+    obj_random_forest = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
+    obj_random_forest.fit(x_training, y_training)
+    with open('data/random_forest.pkl', mode='wb') as f:
+      pickle.dump(obj_random_forest, f)
+  else:
+    with open('data/random_forest.pkl', 'rb') as f:
+      obj_random_forest = pickle.load(f)
+
+  prevision_random_forest = obj_random_forest.predict(x_test)
+  importances = pd.Series(
+    data=obj_random_forest.feature_importances_,
+    index=['ano', 'cidade', 'estado', 'marca', 'modelo']
+  )
+  important = importances.to_frame()
+  important.reset_index(inplace=True)
+  important.columns = ['Importância','Feature', ]
+  st.markdown('##### Gráfico de Importância de parametros')
+  st.plotly_chart(px.bar(data_frame=important, x='Feature', y='Importância', orientation='h', template='plotly_dark'))
+  table_report(y_test, prevision_random_forest, 'Random Forest')
+  confusion_graph(y_test, prevision_random_forest, 'Random Forest')
+  #matrix(X_training, X_test, y_training, y_test, mtd=obj_random_forest)
+
+
+
+def KNN(
+  x_training: np.ndarray, y_training: np.ndarray, x_test: np.ndarray, y_test: np.ndarray
+  )-> None:
+  st.markdown('### Resultado do machine learning usando o método KNN')
+  if not(os.path.isfile('data/KNN_data.pkl')):
+    obj_knn = KNeighborsClassifier(n_neighbors=10, weights='distance', p=1)
+    obj_knn.fit(x_training, y_training)
+    with open('data/KNN_data.pkl', mode='wb') as f:
+      pickle.dump(obj_knn, f)
+  else:
+    with open('data/KNN_data.pkl', 'rb') as f:
+      obj_knn = pickle.load(f)
+  prevision_knn = obj_knn.predict(x_test)
+  table_report(y_test, prevision_knn, 'KNN')
+  confusion_graph(y_test, prevision_knn, 'KNN')
+
+
+
+
+
+
+if not(os.path.isfile('data/price_cars.pkl')):
+  print('iniciando...')
+  main()
+
+with open('data/price_cars.pkl', 'rb') as f:
+    X_training, X_test, y_training, y_test = pickle.load(f)
+
+    
+naive_bayes(X_training, y_training, X_test, y_test)
+tree_decision(X_training, y_training, X_test, y_test)
+random_forest(X_training, y_training, X_test, y_test)
+KNN(X_training, y_training, X_test, y_test)
+
+
+#transform_pkl.py
+
+columns_drop = ['preco','preco_intervalo','cidade','quilometragem','preco_ajustado','preco_depreciado']
+# definindo as features e target
+def features_and_target(data: pd.DataFrame, target: str, columns_drop):
+  X_data= data.drop(columns_drop, axis=1)
+  y_data= data[target]
+  return X_data, y_data
+
+def standard(x_data):
+  scaler = StandardScaler()
+  x_data = scaler.fit_transform(x_data)
+
+  return x_data
+
+
+def onehot_encoder(x_label: np.ndarray, columns: List[int]) -> np.ndarray:
+  onehot = ColumnTransformer(transformers=[(
+    'OneHot', 
+    OneHotEncoder(), 
+    columns,
+    )], remainder='passthrough')
+  x = onehot.fit_transform(x_label).toarray()
+  return x
+
+
+def pre_processing(data):
+  X_data, y_data = features_and_target(data,'preco_intervalo',columns_drop)
+  le=LabelEncoder()
+  for col in X_data:
+    if X_data[col].dtypes == 'object':
+      X_data[col] = pd.DataFrame(le.fit_transform(X_data[col]))
+  
+  X_data = X_data.values
+  y_data = y_data.values
+  X_data = standard(X_data)
+
+  return X_data, y_data
+
+def save_pkl(
+  x_data: np.ndarray, y_data: np.ndarray, path: str = 'data.pkl', per: int =0.2, random: int=0
+  )-> None:
+  '''save(x_training, y_training, x_teste, y_teste)'''
+  X_training, X_test, y_training, y_test = train_test_split(
+  X_data, y_data, test_size=per, random_state=random
+    )
+  with open(path, mode='wb') as f:
+    pickle.dump([X_training, X_test, y_training, y_test], f)
+
+  return X_training, X_test, y_training, y_test
+
+X_data, y_data = pre_processing(data)
+
+def main():
+  if not(os.path.isfile('data/price_cars.pkl')):
+    save_pkl(X_data, y_data,'data/price_cars.pkl')
+  X_training, X_test, y_training, y_test = save_pkl(X_data, y_data,'price_cars.pkl')
+
+main()
+
+with open('data/price_cars.pkl', 'rb') as f:
+    X_training, X_test, y_training, y_test = pickle.load(f)
