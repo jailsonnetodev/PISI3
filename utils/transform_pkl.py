bbfb27f162687e968fa8205df3efe6c95e7fee7b
Ronaldbarbosa00 <157071822+Ronaldbarbosa00@users.noreply.github.com>
2024-09-02 19:34:41 -0300

    Update transform_pkl.py
    Atualização de código

--- utils/transform_pkl.py
+++ utils/transform_pkl.py
@@ -7,7 +7,12 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_parquet('data\dataprocess.parquet')
 
 
-data = pd.read_parquet('data/price_cars.parquet')
-
+columns_drop = ['dias_no_mercado','dias_no_mercado_label']
