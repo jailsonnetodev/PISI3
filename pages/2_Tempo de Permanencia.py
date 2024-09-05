e76274419932b324ea352fd442e186963fd7252e
João Gabriel Nascimento <156119823+Cazeaje@users.noreply.github.com>
2024-09-04 21:12:01 -0300

    Update 2_Tempo de Permanencia.py

--- pages/2_Tempo de Permanencia.py
+++ pages/2_Tempo de Permanencia.py
@@ -10,3 +10,22 @@ build_header(
     hdr='# PRIMEIRAS ANÁLISES E VISUALIZAÇÕES',
     p='<p>Aqui vamos realizar as primeiras observações dos dados e correlações entre algumas variáveis.</p>'
 )
+plt.figure(figsize = (25 , 10))
+sns.heatmap(df.select_dtypes('float' , 'int').corr() , annot = True)
+plt.xticks(rotation = 45);
+
+fig = px.scatter(df, x="preco", y="dias_no_mercado", color="qtd_proprietarios",
+                 hover_data=["nome_modelo", "ano"], title="Tempo de Venda vs. Preço")
+fig.show()
+fig = px.box(df, x="qtd_proprietarios", y="dias_no_mercado",
+            color="qtd_proprietarios", title="Dias no Mercado por Quantidade de Proprietários")
+fig.show()
+ig = px.histogram(df, x="dias_no_mercado", color="preco",
+                  nbins=20, title="Distribuição de Dias no Mercado por Faixa de Preço")
+fig.show()
+fig = px.scatter(df, x="preco", y="dias_no_mercado", color="qtd_proprietarios",
+                 hover_data=["nome_modelo", "ano"], title="Tempo de Venda vs. Preço")
+fig.show()
+
+px.box(df,x='quilometragem',y='dias_no_mercado_label')
+group_by_year = df.groupby(['ano','dias_no_mercado_label']).agg({'preco': np.mean}).reset_index()
