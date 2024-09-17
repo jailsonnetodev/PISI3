b1bcd11110835c9f08af594233f11c58eaf4085c
yrlan777 <95425342+yrlan777@users.noreply.github.com>
2024-09-17 14:56:46 -0300

    Update 5_Permanencia x Quilometragem.py

--- pages/5_Permanencia x Quilometragem.py
+++ pages/5_Permanencia x Quilometragem.py
@@ -6,3 +6,30 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from utils.build import build_header, top_categories
 from utils.graph import boxplot, line_graph
+
+
+build_header(
+    title='Analise Dos Precos',
+    hdr='# ANALISE DA QUILOMETRAGEM ',
+    p='''
+        <p> Aqui vamos realizar as primeiras observações dos dados e correlações entre algumas variaveis</p>
+    '''
+
+)
+
+data = pd.read_parquet('data/usedcars_usa.parquet')
+
+fig8 = px.box(data,x='quilometragem',y='dias_no_mercado_label')
+fig8
+
+group_km =  data.groupby(['dias_no_mercado'])['quilometragem'].size().reset_index()
+fig10 = px.line(group_km, x='dias_no_mercado',y='quilometragem')
+fig10
+
+
+
+
+group_ano_km_preco = data.groupby(['ano','quilometragem'])['preco'].mean().reset_index()
+fig12 = px.bar(group_ano_km_preco, x="ano", y=["quilometragem", "preco"], barmode='group',
+            title="Total de Veículos e Preço Médio por Ano")
+st.plotly_chart(fig12)
