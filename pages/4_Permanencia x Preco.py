b450ce8f27fde567ec7bf50cd63c2d05fbe3f563
yrlan777 <95425342+yrlan777@users.noreply.github.com>
2024-09-17 14:54:37 -0300

    Update 4_Permanencia x Preco.py

--- pages/4_Permanencia x Preco.py
+++ pages/4_Permanencia x Preco.py
@@ -15,6 +15,23 @@ build_header(
     p='''
         <p> Aqui vamos realizar as primeiras observações dos dados e correlações entre algumas variaveis</p>
     '''
-
 )
-data = pd.read_parquet('data/price_cars100k.parquet')
+data = pd.read_parquet('data/usedcars_usa100k.parquet')
+
+
+
+
+df_preco_ano = data.groupby('ano')['preco'].mean().reset_index()
+fig6 = px.line(df_preco_ano, x='ano', y='preco', title='Preço médio por ano')
+st.plotly_chart(fig6)
+
+
+
+group_make =  data.groupby(['nome_marca'])['preco'].size().reset_index().sort_values(by='preco',ascending=False)
+
+fig11 = px.bar(group_make, x='nome_marca', y='preco')
+fig11
+
+group_by_year = data.groupby(['ano','dias_no_mercado_label']).agg({'preco': np.mean}).reset_index()
+fig9 = px.line(group_by_year, x='ano', y='preco', color='dias_no_mercado_label')
+fig9
