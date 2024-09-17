1cab27c52c6d06ba5bd39f773e494785976dfbd9
yrlan777 <95425342+yrlan777@users.noreply.github.com>
2024-09-17 14:52:39 -0300

    Update 2_Tempo de Permanencia.py

--- pages/2_Tempo de Permanencia.py
+++ pages/2_Tempo de Permanencia.py
@@ -1,48 +1,101 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
-from utils.build import build_header
+from utils.build import *
 from utils.graph import boxplot, scatter, hist
 
 build_header(
     title='Primeiras Análises',
-    hdr='# PRIMEIRAS ANÁLISES E VISUALIZAÇÕES',
-    p='<p>Aqui vamos realizar as primeiras observações dos dados e correlações entre algumas variáveis.</p>'
+    hdr='# Tempo de Permanencia do Veiculo no Mercado de Usados',
+    p='<p>Neste trabalho será possivel identificar as caracteristicas fisicas, funcionais e nao funcionais do veiculo com objetivo indeiticar esse fatores que influencia o tempo de permancencia do veiculo listado no site e disponivel no mercado.</p>'
 )
-plt.figure(figsize = (25 , 10))
-sns.heatmap(df.select_dtypes('float' , 'int').corr() , annot = True)
+
+data = pd.read_parquet('data/usedcars_usa.parquet')
+
+
+col1,col2 = st.columns([0.3,0.8])
+
+with col1:
+    top_categorics = st.slider('Selecione quantidade de marcas que deseja visualizar:', 0, 20, 5)
+with col2:
+    data_filtered = top_categories(
+    data = data,
+    top=top_categorics,
+    label='nome_marca'
+    )
+    new_fig = px.bar(data_filtered, x='nome_marca', y='preco')
+    new_fig
+
+
+group_make =  data.groupby(['nome_marca'])['preco'].mean().reset_index().sort_values(by='preco',ascending=False)
+
+
+
+fig =plt.figure(figsize = (25 , 10))
+sns.heatmap(data.select_dtypes('float' , 'int').corr() , annot = True)
 plt.xticks(rotation = 45);
+st.pyplot(fig)
 
-fig = px.scatter(df, x="preco", y="dias_no_mercado", color="qtd_proprietarios",
-                 hover_data=["nome_modelo", "ano"], title="Tempo de Venda vs. Preço")
-fig.show()
-fig = px.box(df, x="qtd_proprietarios", y="dias_no_mercado",
-            color="qtd_proprietarios", title="Dias no Mercado por Quantidade de Proprietários")
-fig.show()
-ig = px.histogram(df, x="dias_no_mercado", color="preco",
-                  nbins=20, title="Distribuição de Dias no Mercado por Faixa de Preço")
-fig.show()
-fig = px.scatter(df, x="preco", y="dias_no_mercado", color="qtd_proprietarios",
-                 hover_data=["nome_modelo", "ano"], title="Tempo de Venda vs. Preço")
-fig.show()
 
-px.box(df,x='quilometragem',y='dias_no_mercado_label')
-group_by_year = df.groupby(['ano','dias_no_mercado_label']).agg({'preco': np.mean}).reset_index()
+df_ano = data.groupby('ano')['dias_no_mercado'].mean().reset_index()
+fig = px.line(df_ano, x='ano', y='dias_no_mercado',
+              title='Relação entre ano de fabricação e tempo de permanência')
+st.plotly_chart(fig)
 
-px.line(group_by_year, x='ano', y='preco', color='dias_no_mercado_label')
-group_km =  df.groupby(['dias_no_mercado_label'])['quilometragem'].size().reset_index()
-px.line(group_km, x='dias_no_mercado_label',y='quilometragem')
-group_km =  df.groupby(['dias_no_mercado'])['quilometragem'].size().reset_index()
-px.line(group_km, x='dias_no_mercado',y='quilometragem')
-group_by_price_year =  df.groupby(['ano'])['preco'].mean().reset_index()
-px.line(group_by_price_year, x='ano',y='preco')
-group_km =  df.groupby(['dias_no_mercado'])['quilometragem'].size().reset_index()
-px.line(group_km, x='dias_no_mercado',y='quilometragem')
-px.scatter(df, x='dias_no_mercado',y='quilometragem', color='ee_novo')
-group_make =  df.groupby(['nome_marca'])['preco'].size().reset_index().sort_values(by='preco',ascending=False)
-px.bar(group_make, x='nome_marca', y='preco')
-fig = px.scatter(df[['dias_no_mercado','preco']], x="dias_no_mercado", y="preco",size="preco",size_max=60)
-fig.show()
-group_cor =  df.groupby(['cor_listagem']).size().reset_index(name='Total')
-px.bar(group_cor, x='cor_listagem', y='Total')
+
+df_acidentes = data.groupby('historico_acidente')['dias_no_mercado'].mean().reset_index()
+fig2 = px.bar(df_acidentes, x='historico_acidente', y='dias_no_mercado',
+             title='Impacto de histórico de acidentes no tempo de venda')
+st.plotly_chart(fig2)
+
+
+
+df_cor = data.groupby('cor_exterior')['dias_no_mercado'].mean().reset_index()
+
+datafiltered = top_categories(
+    data=df_cor,
+    top=10,
+    label='cor_exterior'
+)
+fig3 = px.bar(datafiltered, x='cor_exterior', y='dias_no_mercado',
+             title='Relação entre cor do veículo e tempo de venda')
+st.plotly_chart(fig3)
+
+
+
+df_combustivel = data.groupby('tipo_combustivel')['preco'].mean().reset_index()
+fig4 = px.bar(df_combustivel, x='tipo_combustivel', y='preco', title='Preferência por tipo de combustível')
+title=('Relação entre cor do veículo e tempo de venda')
+st.plotly_chart(fig4)
+
+
+df_veiculos_ano = data.groupby(['ano','dias_no_mercado_label']).size().reset_index(name='total_veiculos')
+fig5 = px.bar(df_veiculos_ano, x='ano', y='total_veiculos', title='Total de veículos por ano', color='dias_no_mercado_label')
+st.plotly_chart(fig5)
+
+
+
+seats_df = data.groupby(['maximo_assentos' , 'tipo_carroceria'])['tipo_carroceria'].count().to_frame().rename(columns = {'tipo_carroceria':'Count'}).reset_index()
+fig7 = px.bar(seats_df, x="tipo_carroceria", y="Count", animation_frame="maximo_assentos", animation_group="maximo_assentos",
+            color="Count")
+fig7["layout"].pop("updatemenus") # optional, drop animation buttons
+st.plotly_chart(fig7)
+
+
+
+
+
+
+data_make = top_categories(
+    data=data,
+    top=5,
+    label='nome_marca'
+)
+
+
+fig = px.bar(data_make, x='nome_marca', y='dias_no_mercado', color='ano',
+             title='Tempo de permanência no mercado por modelo e ano')
+fig
