e9bdc1e9de41ea523c6cec40bd9fb03966e64ab1
Ronaldbarbosa00 <157071822+Ronaldbarbosa00@users.noreply.github.com>
2024-09-17 20:33:26 -0300

    Update transform_regressor_pkl.py

--- utils/transform_regressor_pkl.py
+++ utils/transform_regressor_pkl.py
@@ -37,3 +37,44 @@ def onehot_encoder(x_label: np.ndarray, columns: List[int]) -> np.ndarray:
     )], remainder='passthrough')
   x = onehot.fit_transform(x_label).toarray()
   return x
+def pre_processing(data):
+  X_data, y_data = features_and_target(data,'dias_no_mercado',columns_drop)
+  le=LabelEncoder()
+  for col in X_data:
+    if X_data[col].dtypes == 'object':
+      X_data[col] = pd.DataFrame(le.fit_transform(X_data[col]))
+
+  X_data = X_data.values
+  y_data = y_data.values
+  X_data = standard(X_data)
+
+  return X_data, y_data
+
+
+
+
+def save_pkl(
+  x_data: np.ndarray, y_data: np.ndarray, path: str = 'data_regressor.pkl', per: int =0.2, random: int=0
+  )-> None:
+  '''save(x_training, y_training, x_teste, y_teste)'''
+  X_training, X_test, y_training, y_test = train_test_split(
+  x_data, y_data, test_size=per, random_state=random
+    )
+  with open(path, mode='wb') as f:
+    pickle.dump([X_training, X_test, y_training, y_test], f)
+
+  return X_training, X_test, y_training, y_test
+
+X_data_regressor, y_data_regressor = pre_processing(df)
+
+
+
+def main():
+  if not(os.path.isfile('usedcars_usa_regressor.pkl')):
+    save_pkl(X_data_regressor, y_data_regressor,'usedcars_usa_regressor.pkl')
+  X_training_regressor, X_test_regressor, y_training_regressor, y_test_regressor = save_pkl(X_data_regressor, y_data_regressor,'usedcars_usa_regressor.pkl')
+
+main()
+
+with open('usedcars_usa_regressor.pkl', 'rb') as f:
+    X_training_regressor, X_test_regressor, y_training_regressor, y_test_regressor = pickle.load(f)
