ea91c8ea0342743385cb2c357f4e35de6901f8a0
Ronaldbarbosa00 <157071822+Ronaldbarbosa00@users.noreply.github.com>
2024-09-20 20:37:27 -0300

    Update 2_Tempo de Permanencia.py

--- pages/2_Tempo de Permanencia.py
+++ pages/2_Tempo de Permanencia.py
@@ -13,3 +13,23 @@ build_header(
     p='<p>Neste trabalho será possivel identificar as caracteristicas fisicas, funcionais e nao funcionais do veiculo com objetivo indeiticar esse fatores que influencia o tempo de permancencia do veiculo listado no site e disponivel no mercado.</p>'
 )
 
+data = pd.read_parquet('data/usedcars_usa100k.parquet')
+
+
+col1,col2 = st.columns([0.3,0.8])
+
+with col1:
+    top_categorics = st.slider('Selecione quantidade de marcas que deseja visualizar:', 0, 20, 5)
+with col2:
+    data_filtered = top_categories(
+    data = data,
+    top=top_categorics,
+    label='nome_marca'
+    )
+    bar(
+      data=data_filtered,
+      x='nome_marca',
+      y='preco',
+      title='Grafico de Barras da Media de Preco por Marca',
+      p='<p></p>'
+    )
